e2a5e24f9dcc53c315c4ad1c59ef845eb250ffb1
Marius <marius@example.com>
2022-10-14 10:18:06 +0300

    init

---
+++ js/uzd.js
@@ -0,0 +1,11 @@
+'use strict';
+console.log('uzd.js');
+
+// 13. atspausdinti konsoleje skaicius nuo 12 iki 24 padaugintus is 3
+
+// 1. sukam cikla nuo 12 iki 24
+// 2. kiekvieno ciklo metu mes einamaja reiksme (index) padauginam is 3 ir atspausdinam
+
+// 20. sudauginti skaiciu nuo kintamojo min iki kintamojo max
+// min 5. max 10
+// sudauginti ir aspausdinti skaiciu daugybos rezultata
