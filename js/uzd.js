264205f67a250d746d35d580cf59ca765db1bcf7
Marius <marius@example.com>
2022-10-14 10:22:35 +0300

    padaugintas

--- js/uzd.js
+++ js/uzd.js
@@ -5,6 +5,10 @@ console.log('uzd.js');
 
 // 1. sukam cikla nuo 12 iki 24
 // 2. kiekvieno ciklo metu mes einamaja reiksme (index) padauginam is 3 ir atspausdinam
+for (let i = 12; i <= 24; i++) {
+  let padaugintas = i * 3;
+  console.log(`reiksme ${i} padauginta gaunasi ${padaugintas}`);
+}
 
 // 20. sudauginti skaiciu nuo kintamojo min iki kintamojo max
 // min 5. max 10
